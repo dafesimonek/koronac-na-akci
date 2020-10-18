5582e19cc9cad4d90c7155957202efad97aa7b25
dafesimonek <dafesimonek@gmail.com>
2020-10-18 15:39:36 +0200

    app title

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -11,7 +11,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Koronáč na akci',
       theme: buildTheme(),
       initialRoute: "/",
       routes: applicationRoutes,
